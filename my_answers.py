488af125156b04c02eda658702ab7f8e22f12065
yajneshvar <yajneshvar.arumugam@gmail.com>
2017-10-15 16:09:13 +0000

    Removing drop out to make it moreaccurate

--- my_answers.py
+++ my_answers.py
@@ -62,7 +62,7 @@ def window_transform_text(text, window_size, step_size):
 # a single LSTM hidden layer with softmax activation, categorical_crossentropy loss 
 def build_part2_RNN(window_size, num_chars):
     model = Sequential()
-    model.add(LSTM(200,input_shape=(window_size,num_chars),dropout=0.2,recurrent_dropout=0.2))
+    model.add(LSTM(200,input_shape=(window_size,num_chars)))
     model.add(Dense(num_chars))
     model.add(Activation('softmax'))
     return model
